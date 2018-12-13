fa588616bf5fb4101de2c2e00b148b1ca350170e
Rodrigo <rodrigo.aguilar@globant.com>
2018-12-13 12:12:04 -0500

    chore(): changing Dockerfile

--- src/config/server.js
+++ src/config/server.js
@@ -7,7 +7,7 @@ const router     = require('./router');
 // require('./database');
 
 const app        = express();
-const port       = process.env.PORT || 8080;
+const port       = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
